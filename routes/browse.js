33880960b4fd9806c6e998965f9586bfe051dbfd
xiao <1262260527@qq.com>
2022-06-06 18:08:12 +0800

    fix: update

--- routes/browse.js
+++ routes/browse.js
@@ -8,17 +8,22 @@ router.post('/gallery-photo-list', function (ctx, next) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
   }
-
+  const getRandomColor = () =>
+    '#' +
+    ('00000' + ((Math.random() * 0x1000000) << 0).toString(16)).substr(-6);
   ctx.body = {
     retCode: 0,
     total: 5,
-    list: new Array(Number(200)).fill(null).map((_, index) => {
+    list: new Array(Number(70)).fill(null).map((_, index) => {
       const width = 400 + getRandomIntInclusive(20, 500);
       const height = 300 + getRandomIntInclusive(10, 1000);
       return {
+        id: `${index}`,
+        userId: `${index}`,
         title: `Title${index + 1}`,
         description: `description123123211`,
         src: `https://via.placeholder.com/${width}x${height}`,
+        // src: `https://www.dmoe.cc/random.php`,
         width,
         height,
         placeholderSrc: `https://via.placeholder.com/${width}x${height}?text=placeholder`,
@@ -28,6 +33,7 @@ router.post('/gallery-photo-list', function (ctx, next) {
         answerCount: 20 + index,
         date: new Date(),
         dateStr: '2022年5月21日 21:00:21',
+        subjectColor: getRandomColor(),
       };
     }),
   };
